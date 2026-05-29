90ffdc12a714750b00c14552b1f8690825fdb4ea
PeterTZN <jeep11@hotmail.com>
2026-05-29 19:26:43 +0100

    Package working.

---
+++ main.py
@@ -0,0 +1,6 @@
+#test file to test package (invoice)
+from invoicing import generate
+
+generate("invoices", "output", "pythonhow.png",
+                 "product_id","product_name", "amount_purchased",
+                 "price_per_unit", "total_price")
